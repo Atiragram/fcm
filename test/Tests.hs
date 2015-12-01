800092fa1ca387434d23b51294ac5c1b1edd0aa6
Margosha <margosha058@gmail.com>
2015-12-01 17:25:57 +0300

    Init

--- test/Tests.hs
+++ test/Tests.hs
@@ -11,7 +11,7 @@ main = do
       test3 = TestCase (assertEqual "(evklidDistance [0.0,0.0,0.0] [0.0,0.0,0.0])" 0.0 (evklidDistance (V.fromList [0.0,0.0,0.0]) (V.fromList [0.0,0.0,0.0])))
       test4 = TestCase (assertEqual "(evklidDistance [0.0,0.0] [3.0,0.0])" 3.0 (evklidDistance (V.fromList [0.0,0.0]) (V.fromList [3.0,0.0])))
       test5 = TestCase (assertEqual "(normMatrix [[0.0,0.0,0.0],[0.0,0.0,0.0]] [[0.0,0.0,0.0],[0.0,0.0,0.0]])" 0.0 (normMatrix (V.fromList $ Prelude.map V.fromList [[0.0,0.0,0.0],[0.0,0.0,0.0]]) (V.fromList $ Prelude.map V.fromList [[0.0,0.0,0.0],[0.0,0.0,0.0]])))
-      test6 = TestCase (assertEqual "(normMatrix [[2.0,3.0],[11.0,10.0]] [[6.0,7.0],[21.0,11.0]])" 10.0 (normMatrix (V.fromList $ Prelude.map V.fromList [[1.0,2.0],[3.0,4.0]]) (V.fromList $ Prelude.map V.fromList [[5.0,6.0],[7.0,8.0]])))
+      test6 = TestCase (assertEqual "(normMatrix [[2.0,3.0],[11.0,10.0]] [[6.0,7.0],[21.0,11.0]])" 10.0 (normMatrix (V.fromList $ Prelude.map V.fromList [[2.0,3.0],[11.0,10.0]]) (V.fromList $ Prelude.map V.fromList [6.0,7.0],[21.0,11.0]])))
       testList = TestList [test1, test2, test3, test4, test5, test6]
   counts <- runTestTT testList
   return ()
